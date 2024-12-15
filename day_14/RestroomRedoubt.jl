68b7482fc6a866ef9077b746c04bc212bb290227
Wayne Stegner <wayne.stegner@protonmail.com>
2024-12-15 03:27:31 -0500

    Add day 14 part 2

--- day_14/RestroomRedoubt.jl
+++ day_14/RestroomRedoubt.jl
@@ -4,9 +4,22 @@
 # Input Processing #
 ####################
 
+struct Robot
+  pos::Vector{Int}
+  vel::Vector{Int}
+end
+
+function parseline(l::AbstractString)
+  pos = parse.(Int, match(r"p=(-?\d+),(-?\d+)", l).captures)
+  vel = parse.(Int, match(r"v=(-?\d+),(-?\d+)", l).captures)
+  return Robot(pos, vel)
+end
+
 input = open(string(@__DIR__, "/input.txt")) do f
   split(read(f, String), "\n")[1:end-1]
 end
+
+global robots::Vector{Robot} = map(parseline, input)
 
 ##########
 # Part 1 #
@@ -14,9 +27,9 @@ end
 
 function printfacility(f::Matrix{Int})
   # Flip rows/cols to match problem description for easier visual comparison
-  for c in eachcol(facility)
+  for c in eachcol(f)
     for r in c
-      print(r>0 ? r : '.')
+      print(r>0 ? r : ' ')
     end
     println()
   end
@@ -32,37 +45,57 @@ end
 #const facility_size = [11, 7]
 # Real Facility
 const facility_size = [101, 103]
-facility = zeros(Int, Tuple(facility_size))
-quadrants = zeros(Int, (2,2))
-quadrant_bounds = ceil.(Int,facility_size / 2)
 const max_sim_time = 100
 
-pos_regex = r"p=(-?\d+),(-?\d+)"
-vel_regex = r"v=(-?\d+),(-?\d+)"
-
-for robot in input
-  init_pos = parse.(Int, match(pos_regex, robot).captures)
-  vel = parse.(Int, match(vel_regex, robot).captures)
-  final_pos = init_pos + max_sim_time*vel
-  final_pos = wrappos(final_pos, facility_size)
-  if sum(final_pos .== quadrant_bounds) > 0
-    continue
+function simtotime(time::Int, anomaly_threshold::Float64=0.0)::Int
+  global robots
+  facility = zeros(Int, Tuple(facility_size))
+  quadrants = zeros(Int, (2,2))
+  quadrant_bounds = ceil.(Int,facility_size / 2)
+  for robot in robots
+    final_pos = robot.pos + time*robot.vel
+    final_pos = wrappos(final_pos, facility_size)
+    # Tracking the facility is for debug display only
+    facility[CartesianIndex(Tuple(final_pos))] += 1
+    if sum(final_pos .== quadrant_bounds) > 0
+      continue
+    end
+    quadrant = div.(final_pos, quadrant_bounds)
+    quadrants[CartesianIndex(Tuple(quadrant+[1,1]))] += 1
   end
-  quadrant = div.(final_pos, quadrant_bounds)
-  quadrants[CartesianIndex(Tuple(quadrant+[1,1]))] += 1
-  # Tracking the facility is for debug display only
-  facility[CartesianIndex(Tuple(final_pos))] += 1
+  safety_factor = 1
+  for quadrant in quadrants
+    safety_factor *= quadrant
+  end
+  # Only print anomalous entries
+  if safety_factor < anomaly_threshold
+    printfacility(facility)
+  end
+  return safety_factor
 end
 
-#printfacility(facility)
-
-safety_factor = 1
-for quadrant in quadrants
-  global safety_factor *= quadrant
-end
-
-println("Safety factor: $(safety_factor)")
+println("Safety factor: $(simtotime(max_sim_time))")
 
 ##########
 # Part 2 #
 ##########
+
+#using Plots
+using Statistics
+
+safety_factors = map(simtotime, 1:10000)
+tree_time = argmin(safety_factors)
+# Setting anomaly_threshold to Inf makes it always print
+#simtotime(tree_time, Inf)
+println("Tree at: $(tree_time)")
+
+# I did some work to look at how the safety factor changes over time and noticed
+# some anomalous entries. I manually iterated all of the anomalous entries for a
+# while, but it was taking too long. So I just guessed it was less than 10k
+# seconds and would be the absolute lowest safety factor, and that happened to
+# be right.
+# I manually iterated over all anomalous entries and verified that the tree is
+# the lowest safety factor. This automated solution gets it.
+#
+#plot(safety_factors, show=true)
+#anomaly_threshold = mean(safety_factors) - 3*std(safety_factors)
